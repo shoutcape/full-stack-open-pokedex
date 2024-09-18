2241ad7f4696136bff288f394f4995e4dfea8066
Shoutcape <kautiainen.ville@gmail.com>
2024-09-18 13:10:09 +0300

    changed ip to localhost

--- playwright.config.js
+++ playwright.config.js
@@ -25,7 +25,7 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://192.168.1.152:8080',
+    baseURL: 'http://localhost:8080',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -72,7 +72,7 @@ module.exports = defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm start',
-    url: 'http://192.168.1.152:8080',
+    url: 'http://localhost:8080',
     reuseExistingServer: !process.env.CI,
   },
 })
