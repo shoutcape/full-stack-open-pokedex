e36e783abf9fd39f02d9cef087b4d2cb01f46783
Shoutcape <kautiainen.ville@gmail.com>
2024-09-19 20:36:18 +0300

    added canary and healthcheck

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  tHIS_IS_AN_ERRROR;
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
+})
+
+app.get('/health', (_req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
+  res.send('ok')
 })
